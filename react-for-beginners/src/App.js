ab682e9b855da1ff21ca4b1f5bdd2e1f97ec1865
4everWeb <wone45@hanamil.net>
2023-04-06 18:12:52 +0900

    miv

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,26 +1,15 @@
-
-import {useState,useEffect} from "react"
-
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Fetail from "./routes/Fetail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading,setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {setCoins(json);
-    setLoading(false)});
-  }, [])
   return (
-   <div>
-    <h1> The Coins! { loading? "": `(${coins.length})`}</h1>
-    {loading ? (<strong>Loading...</strong> ): 
-    <select>
-      {coins.map((coins)=> <option>{coins.name} ({coins.symbol}): {coins.quotes.USD.price}</option>)}
-    </select>
-    }
-    </div>
+    <Routes>
+      <Route path="/movie/:id" element={<Fetail />}></Route>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
   );
 }
 
-export default App;
+export default App();
---
+++ react-for-beginners/src/components/Movie.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Movie({ coverImg, title, summary, genres, id }) {
+  return (
+    <div key={id}>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Movie;
